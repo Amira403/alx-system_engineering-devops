f4f1cd028d3213903e6d753932ec946abb079691
root <root@Mustafa.localdomain>
2023-11-28 23:40:28 +0200

    hi im in bin

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+<<<<<<< HEAD
+puts ARGV[0].scan(/[A-Z]+/).join
+=======
+puts ARGV[0].scan(/[A-Z]*/).join
+>>>>>>> origin/main
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+0x06-regular_expressions
+=======
+ Regular expression
+>>>>>>> origin/main
